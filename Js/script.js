78f0a494a8c223bd24b66748cc6e8143f812274c
Kalid Mohammed <delahk424@gmail.com>
2022-09-07 13:49:29 +0300

    edited

--- Js/script.js
+++ Js/script.js
@@ -1,6 +1,3 @@
-const paginationEl = document.querySelector('.pagination');
-const imageCard1El = document.querySelector('.image-card1-open');
-const imageCard2El = document.querySelector('.image-card2-close');
 const navContainer = document.querySelector('.nav-container');
 const humnavbarsCont = document.querySelector('.hum-navbars');
 const closeSign = document.querySelector('.close-sign');
@@ -12,51 +9,6 @@ const servicesEl = document.querySelector('.service');
 const dropdownEl = document.querySelector('.dropdown');
 const whoWeAreEl = document.querySelector('.who-we-are');
 const whatWeDoEl = document.querySelector('.what-we-do');
-const pagBtn1 = document.querySelector('#pag-btn1');
-const pagBtn2 = document.querySelector('#pag-btn2');
-const prevBtn1 = document.querySelector('#prev-btn1');
-const nextBtn2 = document.querySelector('#next-btn2');
-const item = document.querySelector('.item');
-const item1 = document.querySelector('.item-1');
-const item2 = document.querySelector('.item-2');
-const item3 = document.querySelector('.item-3');
-const item4 = document.querySelector('.item-4');
-const item5 = document.querySelector('.item-5');
-const item6 = document.querySelector('.item-6');
-const item7 = document.querySelector('.item-7');
-const item8 = document.querySelector('.item-8');
-
-
-console.log(item);
-// console.log(item2);
-// console.log(item3);
-// console.log(item4);
-// console.log(item5);
-// console.log(item6);
-// console.log(item7);
-// console.log(item8);
-
-//console.log(nextBtn2);
-// console.log(servicesEl);
-
-
-                //DESKTOP PAGINATION
-            // *********************
-pagBtn1.addEventListener('click', () => {
-    pagBtn1.classList.add('active');
-    pagBtn2.classList.remove('active');
-    imageCard1El.classList.replace('section1-close', 'image-card1-open');
-    imageCard2El.classList.replace('image-card2-open', 'image-card2-close');
-
-});
-
-pagBtn2.addEventListener('click', () => { 
-    pagBtn1.classList.remove('active');
-    pagBtn2.classList.add('active');
-    imageCard1El.classList.replace('image-card1-open', 'section1-close');
-    imageCard2El.classList.replace('image-card2-close', 'image-card2-open');
-
-});
 
 humnavbarsCont.addEventListener('click', () => {
     humnavbarsCont.classList.add('hum-navbars-close');
@@ -77,11 +29,6 @@ homeEl.addEventListener('click', () => {
 
 });
 
-// aboutEL.addEventListener('click', () => {
-//     navCont.classList.remove('nav-cont-open');   
-//     closeSign.classList.remove('close-sign-open');
-//     humnavbarsCont.classList.remove('hum-navbars-close');
-// });
 whoWeAreEl.addEventListener('click', () => {
     navCont.classList.remove('nav-cont-open');   
     closeSign.classList.remove('close-sign-open');
@@ -105,9 +52,34 @@ dropdownEl.addEventListener('click', () => {
     menuEL.classList.toggle('menu-open');
 });
 
+           //SWIPER 
+          //********
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 
-                // MOBILE PAGINATION
-            //******************** 
-nextBtn2.addEventListener('click', () => {
-    item2.classList.add('item-2-open');
-})
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+});
